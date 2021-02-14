dd58a01e07e1f0e105a6372f0197879b1d611a31
Liron Wassa <khruiuuxv@gmail.com>
2021-02-14 13:44:07 +0200

    make it better

--- server/dist/routes/user.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-const user_1 = require("../controllers/user");
-const express_1 = require("express");
-const router = express_1.Router();
-router.post('/login', user_1.login);
-module.exports = router;
